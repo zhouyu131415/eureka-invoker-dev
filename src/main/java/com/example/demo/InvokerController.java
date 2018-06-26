f45b4d46051a87cbd1ff6b6643f171203b3ce3b0
zhouyu131415 <40559829+zhouyu131415@users.noreply.github.com>
2018-06-27 00:27:04 +0800

    修改服务调用者

--- src/main/java/com/example/demo/InvokerController.java
+++ src/main/java/com/example/demo/InvokerController.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@Configuration
 public class InvokerController {
 	
 	@Bean
@@ -19,7 +21,8 @@ public class InvokerController {
 	@RequestMapping(value = "/router", method = RequestMethod.GET)
 	public String router() {
 		RestTemplate restTpl = getRestTemplate();
-		String json = restTpl.getForObject("http://FIRST-EUREKA-PROVIDER/person/123456", String.class);
+		String json = restTpl.getForObject("http://first-eureka-provider/person/1", String.class);
+		System.out.println(json);
 		return json;
 	}
 }
